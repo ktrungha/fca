b1522dba0048cbf68b547d30d0d5b277facb405b
Trung Ha <ktrungha@hotmail.com>
2020-05-04 11:55:19 -0400

    initial message for the FCA

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: '50px', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+      <span>Welcome to the website of the <strong>Freeginar Commercial Automation</strong></span>
+      <span>This website is currently under construction</span>
     </div>
   );
 }
